9f123fd1e33c4d65b573504b7c3b02a7f52415b2
Daian Flores <damian.flores@imaginaingenio.com>
2022-10-13 10:06:09 +0200

    Carga Completa

---
+++ UniversityApiBackend/DataAccess/UniversityContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityContext: DbContext
+    {
+        public UniversityContext(DbContextOptions<UniversityContext> options): base(options)
+        {
+
+        }
+
+        //TODO: Add DbSets (Tables of our Database)
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Course>? Courses { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Student>? Students { get; set; }
+        public DbSet<Chapter>? Chapters { get; set; }
+    }
+}
